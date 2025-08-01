0535c683e108c11dbb213fc8626a879487e95721
CauaSean <seancaua25@gmail.com>
2025-08-01 17:54:32 -0300

    adicionados métodos de gravar endereco e telefone de usuário

--- src/main/java/com/caua/usuario/infrastructure/entity/Usuario.java
+++ src/main/java/com/caua/usuario/infrastructure/entity/Usuario.java
@@ -34,7 +34,6 @@ public class Usuario implements UserDetails {
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "usuario_id", referencedColumnName = "id")
-
     private List<Endereco> enderecos;
 
     @OneToMany(cascade = CascadeType.ALL)
